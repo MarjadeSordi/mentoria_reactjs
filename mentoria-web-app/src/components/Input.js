6b0b588023a4180bb442e524c4a26fd7c09d034d
Marja de Sordi <66893702+MarjadeSordi@users.noreply.github.com>
2021-04-06 14:36:59 -0300

    commit 06.04

---
+++ mentoria-web-app/src/components/Input.js
@@ -0,0 +1,21 @@
+
+
+const InputForm = () => {
+
+return(
+<div>
+<form>
+  <label>
+    Nome:
+    <input type="text" name="name" />
+    </label>
+   
+
+
+</form>
+
+        </div>
+    )
+}
+
+export default InputForm;

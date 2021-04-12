f8f6a316e0a48fb287c26d2b0f72763115a32ba3
Marja de Sordi <66893702+MarjadeSordi@users.noreply.github.com>
2021-04-12 11:37:47 -0300

    prettier e eslint

--- mentoria-web-app/src/components/OnboardingComponent.js
+++ mentoria-web-app/src/components/OnboardingComponent.js
@@ -7,61 +7,59 @@ import caminho from '../assets/icons/caminho.svg';
 import caminhovolta from '../assets/icons/caminhovolta.svg';
 
 const OnboardingComponent = () => {
+  const [slide, setSlide] = useState('1');
 
-
-
-    const [slide, setSlide] = useState('1');
-
-
-    if (slide === '1') {
-        return (<OnboardingElement
-            img={rafiki}
-            textTitle={'H1 - Headline'}
-            textSubtitle={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
-            icon={caminho}
-            onClickAnterior={() => window.location = ("/home")}
-            textAnterior='Home'
-            onClick={() => setSlide('2')} text='Próximo'
-        />)
-    }
-
-    else if (slide === '2') {
-        return (<OnboardingElement
-            img={amico}
-            textTitle={'H1 - Headline'}
-            textSubtitle={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
-            icon={caminho}
-            iconvolta={caminhovolta}
-            onClickAnterior={() => setSlide('1')}
-            textAnterior='Anterior'
-            onClick={() => setSlide('3')}
-            text='Próximo' />)
-    }
-    else if (slide === '3') {
-
-        return (<OnboardingElement
-            img={pana}
-            textTitle={'H1 - Headline'}
-            textSubtitle={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
-            //finaliza={localStorage.setItem('isAuth', 'true')}
-            icon={caminho}
-            iconvolta={caminhovolta}
-            onClickAnterior={() => setSlide('2')}
-            textAnterior='Anterior'
-            onClick={() => window.location = ("/login")}
-            text='Login' />)
-
-    }
-
+  if (slide === '1') {
     return (
-        <div>
-            {OnboardingComponent()}
-        </div>
+      <OnboardingElement
+        img={rafiki}
+        textTitle={'H1 - Headline'}
+        textSubtitle={
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        }
+        icon={caminho}
+        onClickAnterior={() => (window.location = '/home')}
+        textAnterior="Home"
+        onClick={() => setSlide('2')}
+        text="Próximo"
+      />
     );
+  } else if (slide === '2') {
+    return (
+      <OnboardingElement
+        img={amico}
+        textTitle={'H1 - Headline'}
+        textSubtitle={
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        }
+        icon={caminho}
+        iconvolta={caminhovolta}
+        onClickAnterior={() => setSlide('1')}
+        textAnterior="Anterior"
+        onClick={() => setSlide('3')}
+        text="Próximo"
+      />
+    );
+  } else if (slide === '3') {
+    return (
+      <OnboardingElement
+        img={pana}
+        textTitle={'H1 - Headline'}
+        textSubtitle={
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        }
+        //finaliza={localStorage.setItem('isAuth', 'true')}
+        icon={caminho}
+        iconvolta={caminhovolta}
+        onClickAnterior={() => setSlide('2')}
+        textAnterior="Anterior"
+        onClick={() => (window.location = '/login')}
+        text="Login"
+      />
+    );
+  }
 
-
-
-}
-
+  return <div>{OnboardingComponent()}</div>;
+};
 
 export default OnboardingComponent;

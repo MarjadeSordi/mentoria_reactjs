1d255448529fd2d7855840451601e0e9a3a155ea
Marja de Sordi <66893702+MarjadeSordi@users.noreply.github.com>
2021-04-16 10:41:48 -0300

    input

--- mentoria-web-app/src/styles/components/input.js
+++ mentoria-web-app/src/styles/components/input.js
@@ -1,80 +1,77 @@
 import styled, { css } from 'styled-components';
 import { coresGlobais } from '../style';
 
-<coresGlobais />;
+<coresGlobais />
 
 export const StyleFormShared = css`
-  background-color: ${coresGlobais.lightShade};
-  width: 340px;
-  height: 48px;
-  border-radius: 5px;
-
-  margin-bottom: 5px; ;
-`;
+    background-color:${coresGlobais.lightShade};
+    width: 340px;
+    height: 48px;
+    border-radius: 5px;
+ 
+    margin-bottom: 5px;
+    
+  ;`
 
 export const StyledFormDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 50vh;
-  padding: 20px;
-`;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 50vh;
+    padding: 20px;
+    
+`
 
 export const StyledForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  max-width: 700px;
-  padding: 40px;
-`;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 700px;
+    padding: 40px;
+`
 
 export const StyledInput = styled.input`
-  display: block;
-  border: none;
-  padding: 0 1rem;
-  border: none;
-  border: ${props => (props.error ? '3px solid #EB5757' : '')};
-  border: ${props => (props.check ? '3px solid #27AE60' : '')};
+    display: block;   
+    border:none;
+    padding: 0 1rem;
+    border: none;
+    border: ${(props) => props.error ? '3px solid #EB5757' : ''};
+    border: ${(props) => props.check ? '3px solid #27AE60' : ''};
 
-  ${StyleFormShared}
-`;
+    ${StyleFormShared}
+`
+
 
 export const MsgInputError = styled.div`
-  color: #eb5757;
-  font-size: 12px;
-  font-family: Open Sans;
-  margin-top: 0px;
-`;
+    color: #EB5757;
+    font-size: 12px;
+    font-family: Open Sans;
+    margin-top: 0px;
+`
 
 export const MsgInputCheck = styled.div`
-  color: #27ae60;
-  font-size: 12px;
-  font-family: Open Sans;
-`;
+    color: #27AE60;
+    font-size: 12px;
+    font-family: Open Sans;
+   
+`
 
 export const ImagemError = styled.img`
-  width: 15px;
-  position: absolute;
-  margin: -74px 330px;
-`;
+    width: 15px;
+    position: absolute;
+    margin: -74px 330px;
+`
 
 export const ImagemCheck = styled.img`
-  width: 15px;
-  position: absolute;
-  margin: -74px 330px;
-`;
+    width: 15px;
+    position: absolute;
+    margin: -74px 330px;
+`
 
 export const SelectLabel = styled.select`
-  ${StyleFormShared}
-  padding: 0 1rem 0 1rem;
-  border: none;
-  font-family: 'Source Sans Pro', sans-serif;
-`;
-
-export const SelectLabelDrop = styled.select`
-  ${StyleFormShared}
-  padding: 0 1rem 0 1rem;
-  border: none;
-  font-family: 'Source Sans Pro', sans-serif;
-`;
+    ${StyleFormShared}
+    padding: 0 1rem 0 1rem;
+    border: none;
+    font-family: 'Source Sans Pro', sans-serif;
+`
